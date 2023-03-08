7795cb93c2f095ba6ef229cb65ddc874513ed423
Troels-dat21B <troe1912@dat.stud.kea>
2023-03-08 15:13:22 +0100

    added 'Reverse_Polish_Notation'

---
+++ Reverse_Polish_Notation/Program.cs
@@ -0,0 +1,36 @@
+﻿class ReversePolishNotation{
+
+static void Main() {
+        Stack<double> stack = new Stack<double>();
+        string input = "3 4 + *";
+        string[] tokens = input.Split();
+
+        foreach (string token in tokens) {
+            double value;
+
+            if (double.TryParse(token, out value)) {
+                stack.Push(value);
+            } else {
+                double operand2 = stack.Pop();
+                double operand1 = stack.Pop();
+                switch (token) {
+                    case "+":
+                        stack.Push(operand1 + operand2);
+                        break;
+                    case "-":
+                        stack.Push(operand1 - operand2);
+                        break;
+                    case "*":
+                        stack.Push(operand1 * operand2);
+                        break;
+                    case "/":
+                        stack.Push(operand1 / operand2);
+                        break;
+                }
+            }
+        }
+
+        Console.WriteLine("Result: {0}", stack.Pop());
+    }
+
+}
